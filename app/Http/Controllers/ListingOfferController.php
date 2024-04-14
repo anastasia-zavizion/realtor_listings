59943edc25bef542515520ac43a864c6f01197b4
Nastya Zavizion <nastya.gorobets95@gmail.com>
2024-04-14 12:27:52 +0300

    OfferMade notification

--- app/Http/Controllers/ListingOfferController.php
+++ app/Http/Controllers/ListingOfferController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ListingOfferRequest;
 use App\Models\Listing;
 use App\Models\Offer;
+use App\Notifications\OfferMade;
 use Illuminate\Support\Facades\Auth;
 
 class ListingOfferController extends Controller
@@ -12,7 +13,10 @@ class ListingOfferController extends Controller
     public function store(Listing $listing, ListingOfferRequest $request)
     {
         $this->authorize('view',$listing);
-        $listing->offers()->save(Offer::make($request->validated())->bidder()->associate(Auth::user()));
+        $offer = $listing->offers()->save(Offer::make($request->validated())->bidder()->associate(Auth::user()));
+
+        $listing->owner->notify(new OfferMade($offer));
+
         return redirect()->back()->with('success','Offer was made');
     }
 }
